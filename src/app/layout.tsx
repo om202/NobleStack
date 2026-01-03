1c480b2125472870231316e8f7f72e51ebcca648
Omprakash Sah Kanu <opsworld.g@gmail.com>
2026-01-02 19:50:30 -0700

    use dark by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -244,8 +244,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           <DeviceTracker />
